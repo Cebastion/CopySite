30db2beb104555e72a584f753291da2b867635e3
Cebastion <kdimon2006@gimal.com>
2024-03-24 23:58:43 +0200

    new commit

---
+++ JS/burger.js
@@ -0,0 +1,75 @@
+function replaceContentForMobile() {
+    if (window.innerWidth <= 1025) {
+        // Создаем новый контент для мобильной версии
+        var newMobileContent = `
+        <div class="mobile-header__top">
+              <div class="mobile-header__logo">
+                  <a href="https://ya.ru/">
+                      <img
+                          src="https://yastatic.net/q/logoaas/v2/%D0%AF%D0%BD%D0%B4%D0%B5%D0%BA%D1%81.svg?circle=black&color=000&first=white"
+                          alt="Yandex"
+                          style="height: 28px; width: 28px"
+                      />
+                  </a>
+                  <a href="https://practicum.yandex.ru/">
+                      <img
+                          src="https://yastatic.net/q/logoaas/v2/%D0%9F%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D0%BA%D1%83%D0%BC.svg?color=000"
+                          alt="logo"
+                          style="height: 28px; width: 108px"
+                      />
+                  </a>
+              </div>
+              <button id="burger" type="button" class="prisma-button2 prisma-button2_size_m prisma-button2_view_clear prisma-button2_no-outline prisma-button2_with-icon mobile-menu__open-button header__mobile-menu-button prisma_theme_undefined"><span data-test-id="undefined-content" class="prisma-button2__content"><span class="prisma-button2__icon"><svg class="icon icon-list" width="24" height="24" viewBox="0 0 24 24" fill="none"><rect x="5" y="14" width="14" height="2" rx="1" fill="currentColor"></rect><rect x="5" y="8" width="14" height="2" rx="1" fill="currentColor"></rect></svg></span></span></button>
+              <button id="closeBurger" style="display: none;" type="button" aria-label="Закрыть" class="prisma-button2 prisma-button2_size_m prisma-button2_view_clear prisma-button2_no-outline prisma-button2_with-icon mobile-menu__close-button"><span data-test-id="undefined-content" class="prisma-button2__content"><span class="prisma-button2__icon"><svg class="icon icon-close" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M13.414 12L18 16.586A1 1 0 0116.586 18l-5.39-5.39a.862.862 0 010-1.22L16.585 6A1 1 0 1118 7.414L13.414 12z" fill="currentColor"></path><path d="M10.879 12l-4.586 4.586A1 1 0 107.707 18l5.39-5.39a.862.862 0 000-1.22L7.707 6a1 1 0 00-1.414 1.414L10.879 12z" fill="currentColor"></path></svg></span></span></button>
+          </div>
+          <nav class="mobile-header__navigation" style="display: none;">
+              <li>О курсе</li>
+              <li>Как учим</li>
+              <li>Поддержка</li>
+              <li>Программа</li>
+              <li>Отвечаем на вопросы</li>
+          </nav>
+          <div class="mobile-header__bottom" style="display: none;">
+              <nav class="mobile-header__navigation__buttons">
+                  <button class="mobile-navigation__button">Компаниям</button>
+                  <button class="mobile-navigation__button">Все курсы</button>
+                  <button class="mobile-navigation__button">
+                      Записаться на курс
+                  </button>
+              </nav>
+          </div>
+        `;
+
+        // Заменяем существующий контент на странице
+        var contentHeader = document.getElementById("header");
+        contentHeader.innerHTML = newMobileContent;
+    }
+
+    const burger = document.getElementById("burger");
+    const header = document.getElementById("header");
+    const closeBurger = document.getElementById("closeBurger");
+    const navigationPage = document.querySelector(".mobile-header__navigation");
+    const navigationButton = document.querySelector(".mobile-header__bottom");
+
+    const toggleMenuDisplay = () => {
+        burger.style.display = header.classList.toggle("mobile-header")
+            ? "none"
+            : "block";
+        closeBurger.style.display = header.classList.contains("mobile-header")
+            ? "block"
+            : "none";
+        navigationPage.style.display = header.classList.contains("mobile-header")
+            ? "block"
+            : "none";
+        navigationButton.style.display = header.classList.contains("mobile-header")
+            ? "block"
+            : "none";
+    };
+
+    burger.addEventListener("click", toggleMenuDisplay);
+    closeBurger.addEventListener("click", toggleMenuDisplay);
+}
+
+// Вызываем функцию при загрузке страницы и при изменении размера экрана
+window.addEventListener("resize", replaceContentForMobile);
+replaceContentForMobile();
